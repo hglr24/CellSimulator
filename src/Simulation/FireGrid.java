a315e5a8b91bf86da1552005cbb390746a6fabe8
Lucas Liu <ltl9@duke.edu>
2019-02-02 18:28:15 -0500

    Fire simulation

--- src/Simulation/FireGrid.java
+++ src/Simulation/FireGrid.java
@@ -4,11 +4,47 @@ import java.util.ArrayList;
 
 public class FireGrid extends BasicGrid {
 
-
+    public FireGrid(int height, int width, int[][] initInts, FireRuleSet ruleSet) {
+        this.height = height;
+        this.width = width;
+        FireCell[][] initCells = new FireCell[height][width];
+        for (int k = 0; k < height; k++) {
+            for (int j = 0; j < width; j++) {
+                switch (initInts[k][j]) {
+                    case 0:
+                        initCells[k][j] = new FireCell(k, j, FireState.EMPTY);
+                        break;
+                    case 1:
+                        initCells[k][j] = new FireCell(k, j, FireState.TREE);
+                        break;
+                    case 2:
+                        initCells[k][j] = new FireCell(k, j, FireState.BURNING);
+                        break;
+                }
+            }
+        }
+        this.cells = initCells;
+        this.ruleSet = ruleSet;
+    }
 
     @Override
     public ArrayList findNeighbors(Location location) {
-        return null;
+        ArrayList<Cell> neighbors = new ArrayList<>();
+
+        SquareLocation sl = (SquareLocation) location;
+        int[] x = {-1, 1};
+        for (int i : x) {
+
+            SquareLocation temp = new SquareLocation(sl.getX() + i, sl.getY());
+            if (validLocation(temp))
+                neighbors.add(getCell(temp));
+
+            SquareLocation temp2 = new SquareLocation(sl.getX(), sl.getY() + i);
+            if (validLocation(temp2))
+                neighbors.add(getCell(temp2));
+        }
+
+        return neighbors;
     }
 
 }
---
+++ src/Simulation/FireRuleSet.java
@@ -0,0 +1,38 @@
+package Simulation;
+
+import java.util.List;
+
+import static Simulation.FireState.*;
+
+public class FireRuleSet implements RuleSet {
+
+    private double probCatch;
+
+    public FireRuleSet(double[] parameters) {
+        probCatch = parameters[0];
+    }
+
+    @Override
+    public State applyRules(List<Cell> neighbors, Cell cell, Grid grid) {
+        switch ((FireState) cell.getCurrentState()) {
+            case BURNING:
+                return EMPTY;
+            case EMPTY:
+                return EMPTY;
+            default:
+                boolean fireNear = false;
+                for (Cell c : neighbors) {
+                    if (c.getCurrentState() == BURNING)
+                        fireNear = true;
+                    break;
+                }
+
+                if (fireNear && Math.random() > probCatch) {
+                    return BURNING;
+                } else {
+                    return TREE;
+                }
+        }
+
+    }
+}
