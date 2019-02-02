15d10d0adc5a6ff04380a9023eb25d10504598c8
Ian Hanus <ih52@duke.edu>
2019-02-02 16:58:06 -0500

    Rushed example of how to create grid

--- src/Simulation/SimManager.java
+++ src/Simulation/SimManager.java
@@ -1,4 +1,20 @@
 package Simulation;
 
+import Configuration.SimulationInfo;
+import Configuration.XMLReader;
+
+import java.io.File;
+
 public class SimManager {
+    public static void main(){
+        XMLReader testRead = new XMLReader("SimulationInfo");
+        File dataFile = new File("data\\TestSegregation.xml");
+        SimulationInfo testSim = testRead.getSimulation(dataFile);
+
+        switch(testSim.getTitle()){
+            SegregationRuleSet trythis = new SegrationRuleSet(testSim.getPara())
+    }
+
+
+    }
 }
