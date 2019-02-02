e898b907e756131420319d2d81661fed8327ed99
Lucas Liu <ltl9@duke.edu>
2019-02-02 01:50:39 -0500

    changed Cell interface

--- src/Simulation/PredatorPreyCell.java
+++ src/Simulation/PredatorPreyCell.java
@@ -3,16 +3,11 @@ package Simulation;
 
 public class PredatorPreyCell extends Cell {
 
-    public PredatorPreyCell(int xPosition, int yPosition,  initState){
+    public PredatorPreyCell(int xPosition, int yPosition, PredatorPreyState initState){
         location = new SquareLocation(xPosition, yPosition);
         currentState = initState;
         nextState = initState;
     }
 
-    @Override
-    void findNeighbors() {
-
-
-    }
 
 }
