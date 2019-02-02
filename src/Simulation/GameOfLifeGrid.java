9d7c8fb41a2756250692181b57dfe8e12600aa10
Ian Hanus <ih52@duke.edu>
2019-02-02 11:04:04 -0500

    Temporary file containing constructors for the grid subclasses

--- src/Simulation/GameOfLifeGrid.java
+++ src/Simulation/GameOfLifeGrid.java
@@ -4,14 +4,28 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import static Simulation.GameOfLifeState.DEAD;
+import static Simulation.GameOfLifeState.LIVE;
+
 public class GameOfLifeGrid extends BasicGrid {
     private int height;
     private int width;
     private GameOfLifeCell [][] cells;
 
-    GameOfLifeGrid(int height, int width, GameOfLifeCell[][] initCells){
+    GameOfLifeGrid(int height, int width, int[][] initInts){
         this.height = height;
         this.width = width;
+        GameOfLifeCell[][] initCells = new GameOfLifeCell[height][width];
+        for(int k = 0; k < height; k++){
+            for(int j = 0; j < width; j++){
+                switch(initInts[k][j]){
+                    case 0:
+                        initCells[k][j] = new GameOfLifeCell(k, j, DEAD);
+                    case 1:
+                        initCells[k][j] = new GameOfLifeCell(k, j, LIVE);
+                }
+            }
+        }
         this.cells = initCells;
     }
 
---
+++ src/Simulation/TemporaryGridConstructors.java
@@ -0,0 +1,40 @@
+package Simulation;
+
+import static Simulation.FireState.EMPTY;
+import static Simulation.PercolationState.*;
+import static Simulation.PredatorPreyState.FISH;
+import static Simulation.PredatorPreyState.SHARK;
+
+public class TemporaryGridConstructors {
+    public void checkConstructors(){
+        PercolationCell[][] initCells = new PercolationCell[height][width];
+        for(int k = 0; k < height; k++){
+            for(int j = 0; j < width; j++){
+                switch(initInts[k][j]){
+                    case 0:
+                        initCells[k][j] = new PercolationCell(k, j, OPEN);
+                    case 1:
+                        initCells[k][j] = new PercolationCell(k, j, PERCOLATED);
+                    case 2:
+                        initCells[k][j] = new PercolationCell(k, j, BLOCKED);
+                }
+            }
+        }
+
+        PredatorPreyCell[][] initCells = new PredatorPreyCell[height][width];
+        for(int k = 0; k < height; k++){
+            for(int j = 0; j < width; j++){
+                switch(initInts[k][j]){
+                    case 0:
+                        initCells[k][j] = new PercolationCell(k, j, EMPTY);
+                    case 1:
+                        initCells[k][j] = new PercolationCell(k, j, FISH);
+                    case 2:
+                        initCells[k][j] = new PercolationCell(k, j, SHARK);
+                }
+            }
+        }
+    }
+
+
+}
