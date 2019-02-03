75c2558bb490d59d0e4c7fc2f2aa765c6644e387
Lucas Liu <ltl9@duke.edu>
2019-02-03 00:54:47 -0500

    test implementations

--- src/UIpackage/CellSocietyMain.java
+++ src/UIpackage/CellSocietyMain.java
@@ -46,6 +46,8 @@ public class CellSocietyMain extends Application {
         XMLReader testRead = new XMLReader();
 //        File dataFile = new File("data\\TestSegregation.xml");
         File dataFile = new File("data\\TestGameOfLife.xml");
+        //File dataFile = new File("data\\TestFire.xml");
+        //File dataFile = new File("data\\TestPercolation.xml");
         SimulationInfo testSim = testRead.getSimulation(dataFile);
         Grid gridType = null;
         myShape = testSim.getShape();
@@ -61,6 +63,16 @@ public class CellSocietyMain extends Application {
                 GameOfLifeRuleSet rules2 = new GameOfLifeRuleSet();
                 gridType = new GameOfLifeGrid(testSim.getHeight(),testSim.getWidth(),
                         testSim.getIntegerConfiguration(),rules2);
+                break;
+            case FIRE:
+                FireRuleSet rules3 = new FireRuleSet(testSim.getParameters());
+                gridType = new FireGrid(testSim.getHeight(),testSim.getWidth(),
+                        testSim.getIntegerConfiguration(),rules3);
+                break;
+            case PERCOLATION:
+                PercolationRuleSet rules4 = new PercolationRuleSet();
+                gridType = new PercolationGrid(testSim.getHeight(),testSim.getWidth(),
+                        testSim.getIntegerConfiguration(),rules4);
                 break;
                 //TODO make cases for each sim type
         }
