f456b46cf32f54813744a8a0ed12a13c85d3ef35
Lucas Liu <ltl9@duke.edu>
2019-02-11 00:41:14 -0500

    functional ant

--- src/Simulation/AntRuleSet.java
+++ src/Simulation/AntRuleSet.java
@@ -19,16 +19,26 @@ public class AntRuleSet implements RuleSet {
     @Override
     public State applyRules(List<Cell> neighbors, Cell cell, Grid grid) {
         AntCell antCell = (AntCell) cell;
-        //antCell.diffuse();
+
         antCell.evaluate((AntGrid) grid);
+
+        if(cell.getCurrentState() == AntState.FOOD)
+            return cell.getCurrentState();
+
+        if(cell.getCurrentState() == AntState.HOME)
+            return cell.getCurrentState();
+
+        antCell.diffuse();
 
         if(antCell.hasAnts()){
             if(antCell.hasFoodAnts())
                 return AntState.ANT_WITH_FOOD;
             else
                 return AntState.ANT_NO_FOOD;
+        } else{
+            return AntState.EMPTY;
         }
-    return cell.getCurrentState();
+
 
     }
 
